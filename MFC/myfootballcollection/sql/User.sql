40eabc8b0610df4089ce7ade2ac11855cd47f74b
gromba123 <goncaloromba8@gmail.com>
2025-06-06 15:39:28 +0100

    More on the backend of the app: Started implementing user routing for sign up/in

--- MFC/myfootballcollection/sql/User.sql
+++ MFC/myfootballcollection/sql/User.sql
@@ -2,7 +2,7 @@ drop table if exists "user";
 
 CREATE TABLE "user"
 (
-    user_id     varchar(50) PRIMARY KEY,
+    userId     varchar(50) PRIMARY KEY,
     email       varchar(25) UNIQUE,
     name        varchar(250),
     tag         varchar(25) UNIQUE,
